f37fefe381e2b7cf28bb4e3d7dcd098df055776e
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-15 17:31:07 -0500

    chore(hero): add reactive form demo

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+             <hero-form-reactive3></hero-form-reactive3>`
 })
 export class AppComponent { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- demo/app.module.ts
+++ demo/app.module.ts
@@ -1,9 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+
+import { AppComponent }           from './app.component';
+import { HeroFormReactiveModule } from './reactive/hero-form.module';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    HeroFormReactiveModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
